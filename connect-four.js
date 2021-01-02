03f51913fa7c268bdc12e503a6c6d3d4e4cb8a90
james McBride <james.m.mcbride15@gmail.com>
2021-01-02 12:54:52 -0500

    added css changes

--- connect-four.js
+++ connect-four.js
@@ -1,10 +1,10 @@
-var windowWidth= window.innerWidth
-$("body").css("margin-left", windowWidth*.1)
-$("body").css("width", windowWidth*.8)
-$(".circle").css("width", windowWidth*.64/7)
-
-
-var windowHeight=window.innerHeight
-$("body").css("height", windowWidth*6/7)
-$("body").css("padding-top", windowWidth*1/14)
-$(".row-").css("height", windowHeight*.8/6)
+// var windowWidth= window.innerWidth
+// $("body").css("margin-left", windowWidth*.1)
+// $("body").css("width", windowWidth*.8)
+// $(".circle").css("width", windowWidth*.64/7)
+//
+//
+// var windowHeight=window.innerHeight
+// $("body").css("height", windowWidth*6/7)
+// $("body").css("padding-top", windowWidth*1/14)
+// $(".row-").css("height", windowHeight*.8/6)
